bd495a4dc7ba229c81e9f6c24af4ad8119dbc25c
vinay kumar <vinaykugupta.000@gmail.com>
2021-02-04 14:28:36 +0530

    added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,8 +14,8 @@ const forecast=(latitude,longitude,callback)=>{
     
         callback('Unable to find location.Try another search',undefined)
     }else{
-    
-        callback(undefined,body.current.weather_descriptions[0]+'.It is currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out.')
+          
+        callback(undefined,body.current.weather_descriptions[0]+'.It is currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out.The humidity is '+ body.current.humidity + '%.' )
     }
     
     })
