2d1b4e10fad8720de708bc48a507ae6dc4e8aefd
nik <nikhil0596@gmail.com>
2022-12-25 22:27:37 +0530

    added App name in index file

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
         
     res.render('index',{
-        title:'Weather',
+        title:'Weather App',
         name:'Vinay Kumar'
     })
          
